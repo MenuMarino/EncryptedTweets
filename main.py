c9213412d72a292d1fdc901ae10f4f05e004cef1
MenuMarino <benjamin.diaz@utec.edu.pe>
2021-10-24 17:27:57 -0500

    avance

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import keys_management
 import crypto
+from flask import Flask, request
 
-keys_management.generate_keys()
+app = Flask(__name__)
 
-private_key = keys_management.read_key("private_key.pem")
-public_key = private_key.public_key()
+@app.route('/')
+def landing():
+    return 'Hello world'
 
-keys_management.write_key(public_key, "public.key.pem", False)
+@app.route('/send', methods=['POST'])
+def send():
+    tweet = request.json["tweet"]
+    user = request.json["user"]
+    # key = keys_management.get_key_from_user(user)
+    key = keys_management.read_key("private_key.pem")
+    encrypted_tweet = crypto.encrypt(bytes(tweet, encoding='utf8'), key.public_key())
+    return encrypted_tweet
+
+@app.route('/receive', methods=['GET'])
+def receive():
+    return 'TODO'
+
+@app.route('/generate', methods=['GET'])
+def generate():
+    keys_management.generate_keys()
+    public_key = keys_management.read_key("private_key.pem")
+    # Twitear la public key
+    return 'Keys generadas'
+
+app.run(host='localhost', port=8080, debug=True)
